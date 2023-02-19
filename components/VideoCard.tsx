914e9bea05433eae91f67cdd517704f2bc75c8ff
Amaan <amaansayyad@yahoo.com>
2023-02-19 22:27:14 +0530

    Update VideoCard.tsx v2 commit

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,24 +1,22 @@
 
 import moment from "moment";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { useNFTCollection } from "@thirdweb-dev/react";
-import { PROFILE_NFT_ADDRESS } from "../constants";
-import { NFTMetadataOwner } from "@thirdweb-dev/sdk";
+import useSuperstreamContract from "../hooks/useSuperstreamContract";
+import { Profile } from "../recoil/states";
 
 type Props = {
   data: any;
 };
 
 const VideoCard = ({ data }: Props) => {
-  const profileNft = useNFTCollection(PROFILE_NFT_ADDRESS);
-  const [user, setUser] = useState<NFTMetadataOwner>();
-
+  const superstream = useSuperstreamContract();
+  const [user, setUser] = useState<Profile>();
+ 
   const getUser = async () => {
     if (data?.owner) {
-      const _user = await profileNft.getOwned(data?.owner);
-      setUser(_user[0]);
-      console.log(user);
+      const res = await superstream.getProfileByUsername(data?.creator)
+      setUser(res);
     }
   };
 
@@ -26,27 +24,28 @@ const VideoCard = ({ data }: Props) => {
     getUser();
   }, [data]);
 
+
   return (
     <div className="w-full">
       {/* VideoCard */}
-      <Link href={`/video?id=${data?.metadata.id.toString()}`}>
-        <div className="p-3 rounded-lg  group hover:scale-105 hover:shadow-xl duration-200 ease-out cursor-pointer  hover:bg-gray-800 max-w-sm">
-          <div className="animate-pulse rounded-lg relative aspect-video mb-2 bg-slate-500  w-full">
-            <img src={data?.metadata?.image} alt="" />
+      <Link href={`/video?id=${data?.id.toString()}`}>
+        <div className="hover:p-3 rounded-lg  group hover:scale-105 hover:shadow-xl duration-200 ease-out cursor-pointer  hover:bg-gray-800 max-w-sm">
+          <div className="rounded-lg overflow-hidden relative aspect-video mb-2 bg-slate-500  w-full">
+            <img src={data?.thumbnail} alt="" />
             <div className="bg-slate-900 text-xs text-white absolute right-2 bottom-2 p-1 rounded-md">
-              {moment
-                .duration(Math.ceil(data?.metadata?.duration / 60) * 60 * 1000)
+              {data?.duration  && moment
+                .duration(Math.ceil(data?.duration / 60) * 60 * 1000)
                 .humanize()}
             </div>
           </div>
           <h6 className="text-lg mb-2  overflow-ellipsis flex whitespace-pre-wrap">
-            {data?.metadata?.name.slice(0, 60) +
-              (data?.metadata?.name.length > 60 ? "..." : "")}
+            {data?.title.slice(0, 60) +
+              (data?.title.length > 60 ? "..." : "")}
           </h6>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <div className="h-10 w-10 ring-1 ring-white overflow-hidden rounded-full bg-gray-600">
-              {user?.metadata?.image && (
-                <img src={user?.metadata?.image} alt="profile-pic" />
+              {user?.pfp && (
+                <img src={"https://ipfs.io/ipfs/" + user?.pfp} alt="profile-pic" />
               )}
             </div>
             <div>
@@ -54,10 +53,10 @@ const VideoCard = ({ data }: Props) => {
                 className="text-gray-
             300 font-display font-bold "
               >
-                {user?.metadata.name || "User"}
+                {user?.username || "User"}
               </p>
               <p className="text-gray-400 text-sm ">
-                {moment(data?.metadata?.created_at).fromNow()}{" "}
+                {moment.unix(data?.createdAt).fromNow()}
               </p>
             </div>
           </div>
