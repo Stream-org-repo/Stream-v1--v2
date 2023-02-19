bbd50aa9156d4632801cc7256d3f0c213b0c870c
Amaan <amaansayyad@yahoo.com>
2023-02-19 14:28:19 +0530

    Update _app.tsx v1 commit original

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-//Authored by Amaan
+
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
@@ -6,9 +6,7 @@ import { Toaster } from "react-hot-toast";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { RecoilRoot } from "recoil";
 
-
-
-function MyApp({ Component, pageProps, web3storageToken }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
         <RecoilRoot>
